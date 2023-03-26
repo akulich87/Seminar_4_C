aaf2c2cca5f8a16452152b4cc0f3da7fe57f67f5
akulich87 <amgrupp_project@mail.ru>
2023-03-26 23:08:19 +0300

    add task3

---
+++ task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число N
+// и выдает произведение чисел от 1 до N
+
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine()); // конвертируем в полноценное число
+int factorial = 1;
+int count = 1; 
+
+while(count<=num)
+{
+  factorial *=count; 
+  count++;
+}
+
+Console.WriteLine($"Произведение чисел равно {factorial}"); 
